dd935697ca4db18fe3a15c117d641054ebb77491
Igor Dyniewski <igor.dyniewski@gmail.com>
2020-11-12 14:40:46 +0100

    Styled components setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
+import React from 'react'
+import App from 'next/app'
+import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+
+// Styles
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// theme
+import theme from '../lib/theme'
 
-export default MyApp
+export default class MyApp extends App {
+    render() {
+        const { Component, pageProps } = this.props
+        return (
+            <ThemeProvider theme={theme}>
+                <Head>
+                    <title>Angula Rank</title>
+                </Head>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        )
+    }
+}
