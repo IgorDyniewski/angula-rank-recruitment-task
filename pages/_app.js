e63d6a85469e448becd72d063aa078130d24fece
Igor Dyniewski <igor.dyniewski@gmail.com>
2020-11-17 16:07:47 +0100

    Added loading bar on route change

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,26 @@ import React from 'react'
 import App from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
+import NProgress from 'nprogress'
+import Router from 'next/router'
 
 // Styles
 import '../styles/globals.css'
 import '../styles/spinner.css'
+import '../styles/nprogress.css'
 
 // theme
 import theme from '../lib/theme'
+
+// Setting up an client side loader
+Router.events.on('routeChangeStart', (url) => {
+    console.log(`Loading: ${url}`)
+    NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => {
+    NProgress.done()
+})
+Router.events.on('routeChangeError', () => NProgress.done())
 
 export default class MyApp extends App {
     render() {
